4965ed76f1e69f9a12afe6f26cae5831dbd366ca
dankarger <karger.dan@gmail.com>
2022-11-04 18:03:46 +0200

    finnish

--- script.ts
+++ script.ts
@@ -3,7 +3,44 @@ const prisma = new PrismaClient()
 
 
 async function main() {
-    const user = await prisma.user.findMany()
+    // await prisma.user.deleteMany()
+    // const preference = await prisma.userPreference.create({
+    //     data: {
+    //         emailUpdate: true
+    //     }
+    // })
+    // console.log(preference)
+
+    const user = await prisma.user.update({
+        where: {
+            email : 'Sally@Test3.com'
+        },
+        data: {
+            userPreference: {
+                disconnect : true
+                //     {
+                //     id : '92631e96-7c08-4dbf-a088-1e9e15121691'
+                // }
+            }
+        }
+    })
+    // const user = await prisma.user.findFirst({
+    //     where: {
+    //         email: "Sally@Test3.com"
+    //     }
+    // })
+    // const user =    await prisma.user.createMany({
+    //       data : [{
+    //         name : "Sally",
+    //           email: "Linda@test322.com",
+    //           age: 32,
+    //       },
+    //     {
+    //             name : "Molly3",
+    //             email: "Molly@tes3t232.com",
+    //             age: 57,
+    //         }],
+    //   })
     console.log(user)
 }
 
